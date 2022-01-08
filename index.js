d002a7bfa26229f0305fec12102bec2f7aea6219
aziz <abdulchaman123@gmail.com>
2022-01-08 01:49:28 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Testimonial Swiper Slider
+
+var swiper = new Swiper(".testimonialSwiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 1,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
